4b35dd4d53b8099148c732fdd057f14a20fe10b5
Bryan Aleman <Alemanbryan@ufl.edu>
2024-09-08 22:02:48 -0400

    modified:   Thermostat.vhd

--- Thermostat.vhd
+++ Thermostat.vhd
@@ -1,8 +1,8 @@
 entity THERMO is
-    port( CURRENT_TEMP : in bit_vector(6 downto 0)
-                DESIRED_TEMP : in bit_vector(6 downto 0)
+    port( CURRENT_TEMP : in bit_vector(6 downto 0);
+                DESIRED_TEMP : in bit_vector(6 downto 0);
                 DISPLAY_SELECT: in bit
-                TEMP_DISPLAY: out bit_vector(6 downto 0)
+                TEMP_DISPLAY: out bit_vector(6 downto 0);
                 );
     end THERMO;
     
@@ -11,9 +11,9 @@ entity THERMO is
     process(CURRENT_TEMP, DESIRED_TEMP, DISPLAY_SELECT)
     begin 
         if DISPLAY_SELECT = '1' then
-            TEMP_DISPLAY <= CURRENT_TEMP
+            TEMP_DISPLAY <= CURRENT_TEMP;
         else
-            TEMP_DISPLAY <= DESIRED_TEMP
+            TEMP_DISPLAY <= DESIRED_TEMP;
         end if;
     end process;
     end BEHAV;	
